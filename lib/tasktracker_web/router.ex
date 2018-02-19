7a6c6f414dba9b5655973b39709417602fe32716
David Celentano <davidwcelentano@gmail.com>
2018-02-19 12:12:34 -0500

    logging in supported

--- lib/tasktracker_web/router.ex
+++ lib/tasktracker_web/router.ex
@@ -4,9 +4,17 @@ defmodule TasktrackerWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
+    plug :get_current_user
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+  end
+
+   def get_current_user(conn, _params) do
+    # TODO: Move this function out of the router module.
+    user_id = get_session(conn, :user_id)
+    user = Tasktracker.Accounts.get_user(user_id || -1)
+    assign(conn, :current_user, user)
   end
 
   pipeline :api do
@@ -19,6 +27,8 @@ defmodule TasktrackerWeb.Router do
     get "/", PageController, :index
     resources "/users", UserController
     resources "/tasks", TaskController
+    post "/session", SessionController, :create
+    delete "/session", SessionController, :delete
   end
 
   # Other scopes may use custom stacks.
